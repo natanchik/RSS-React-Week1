ae04bd9d4cbf56171f4aac6e28cfbd8f0721b0b4
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-12-03 13:43:45 +0600

    feat: fill in the form with React Hook Form

--- react-forms/src/App.tsx
+++ react-forms/src/App.tsx
@@ -4,17 +4,15 @@ import './App.scss';
 function App() {
   return (
     <>
-      <h2>Home Page</h2>
+      <h1 className="home__header">Home Page</h1>
       <p className={'home__text'}>
-        Fill out the{' '}
         <Link className={'home__link'} to="/form1">
-          form with React Hook Form
+          Form with React Hook Form
         </Link>
       </p>
       <p className={'home__text'}>
-        Fill out the{' '}
         <Link className={'home__link'} to="/form2">
-          form with uncontrolled components
+          Form with uncontrolled components
         </Link>
       </p>
     </>
--- react-forms/src/pages/Form1.tsx
+++ react-forms/src/pages/Form1.tsx
@@ -1,11 +1,12 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import './forms.scss';
 
 const schema = yup.object().shape({
   name: yup
     .string()
-    .matches(/^A-Z/, {
+    .matches(/^[A-Z]/, {
       message: 'The first letter must be uppercased ',
       excludeEmptyString: true,
     })
@@ -14,15 +15,15 @@ const schema = yup.object().shape({
   email: yup.string().email().required('This field is a required'),
   password: yup
     .string()
-    .matches(/A-Z/, {
+    .matches(/[A-Z]/, {
       message: 'The password must contain an uppercased letter',
       excludeEmptyString: true,
     })
-    .matches(/a-z/, {
+    .matches(/[a-z]/, {
       message: 'The password must contain a lowercased letter',
       excludeEmptyString: true,
     })
-    .matches(/0-9/, {
+    .matches(/[0-9]/, {
       message: 'The password must contain a number',
       excludeEmptyString: true,
     })
@@ -34,8 +35,9 @@ const schema = yup.object().shape({
     .min(6, 'The password must contain at least 6 characters')
     .max(32, 'The password must contain no more than 32 characters')
     .required('This field is a required'),
-  gender: yup.string().required('This field is a required'),
-  acceptTC: yup.string().required('This field is a required'),
+  gender: yup.array().required('This field is a required'),
+  acceptTC: yup.boolean(),
+  country: yup.string().required('This field is a required'),
 });
 
 export default function Form1() {
@@ -47,29 +49,87 @@ export default function Form1() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmitHandler = (data: object) => {
-    console.log({ data });
+  const onSubmitHandler = async (data: object) => {
+    await schema.validate(data);
+    console.log(JSON.stringify({ data }));
     reset();
   };
   return (
-    <form onSubmit={handleSubmit(onSubmitHandler)}>
-      <h2>The form with React Hook Form</h2>
-      <br />
-
-      <input {...register('email')} placeholder="email" type="email" required />
-      <p>{errors.email?.message}</p>
-      <br />
-
+    <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
+      <h2 className="form__header">The form with React Hook Form</h2>
       <input
-        {...register('password')}
-        placeholder="password"
-        type="password"
+        {...register('name')}
+        placeholder="Your name"
+        type="text"
         required
       />
-      <p>{errors.password?.message}</p>
-      <br />
-
-      <button type="submit">Sign in</button>
+      <p className="form__errors">{errors.name?.message}</p>
+      <input {...register('age')} placeholder="Your age" type="text" required />
+      <p className="form__errors">{errors.age?.message}</p>
+      <input
+        {...register('email')}
+        placeholder="E-mail"
+        type="email"
+        autoComplete="username"
+        required
+      />
+      <p className="form__errors">{errors.email?.message}</p>
+      <input
+        {...register('password')}
+        placeholder="Password"
+        type="password"
+        autoComplete="new-password"
+        required
+      />
+      <p className="form__errors">{errors.password?.message}</p>
+      <input
+        {...register('password')}
+        placeholder="Password"
+        type="password"
+        autoComplete="new-password"
+        required
+      />
+      <p className="form__errors">{errors.password?.message}</p>
+      <div className="form__gender">
+        <p>Choose your gender</p>
+        <label>
+          <input
+            {...register('gender')}
+            type="radio"
+            name="gender"
+            value="Male"
+          />
+          Male
+        </label>
+        <label>
+          <input
+            {...register('gender')}
+            type="radio"
+            name="gender"
+            value="Female"
+          />
+          Female
+        </label>
+        <p className="form__errors">{errors.gender?.message}</p>
+      </div>
+      <label className="form__country">
+        Choose country
+        <select {...register('country')}>
+          <option value="">Select...</option>
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <option value="France">France</option>
+        </select>
+      </label>
+      <p className="form__errors">{errors.country?.message}</p>
+      <label className="form__accept">
+        <input {...register('gender')} type="checkbox" name="acceptTC" />
+        Accept T&C
+      </label>
+      <p className="form__errors">{errors.acceptTC?.message}</p>
+      <button className="button" type="submit">
+        Input info
+      </button>
     </form>
   );
 }
