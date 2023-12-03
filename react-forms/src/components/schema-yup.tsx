cacbe98b9f28ea9c56ec2eb1ceac79b1d55735b0
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-12-03 23:41:34 +0600

    refactor: add uncontrolled components

--- react-forms/src/components/schema-yup.tsx
+++ react-forms/src/components/schema-yup.tsx
@@ -4,12 +4,12 @@ export const schema = yup.object().shape({
   name: yup
     .string()
     .matches(/^[A-Z]/, {
-      message: 'The first letter must be uppercased ',
+      message: 'The first letter in name must be uppercased',
       excludeEmptyString: true,
     })
-    .required('This field is a required'),
-  age: yup.number().positive().integer().required('This field is a required'),
-  email: yup.string().email().required('This field is a required'),
+    .required('Name is a required'),
+  age: yup.number().positive().integer().required('Age is a required'),
+  email: yup.string().email().required('E-mail is a required'),
   password: yup
     .string()
     .matches(/[A-Z]/, {
@@ -29,10 +29,10 @@ export const schema = yup.object().shape({
         'The password must contain at least 1 of theese symbols: "!@#$%^&*()_+=-"',
       excludeEmptyString: true,
     })
-    .min(6, 'The password must contain at least 6 characters')
+    .min(8, 'The password must contain at least 8 characters')
     .max(32, 'The password must contain no more than 32 characters')
-    .required('This field is a required'),
-  gender: yup.array().of(yup.string()).required('This field is a required'),
+    .required('The password is a required'),
+  gender: yup.array().of(yup.string()).required('The gender is a required'),
   acceptTC: yup.boolean(),
-  country: yup.string().required('This field is a required'),
+  country: yup.string().required('Country is a required'),
 });
--- react-forms/src/pages/Form2.tsx
+++ react-forms/src/pages/Form2.tsx
@@ -1,100 +1,109 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
 import { schema } from '../components/schema-yup';
 import { useAppDispatch } from '../redux/hooks';
 import { addUser } from '../redux/usersSlice';
 import { useNavigate } from 'react-router-dom';
 import './forms.scss';
+import { FormEvent } from 'react';
 
-export default function Form1() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
+export default function Form2() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const onSubmitHandler = async (data: object) => {
-    dispatch(addUser(data));
-    navigate('/');
+
+  const onSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const entries = [];
+    const inputs = document.querySelectorAll('input');
+    for (const input of inputs) {
+      if (input instanceof HTMLInputElement) {
+        if (input.name !== 'acceptTC') {
+          input.name !== 'gender'
+            ? entries.push([input.name, input.value])
+            : entries.push([input.name, [input.value]]);
+        }
+      }
+    }
+    const select = document.querySelector('select');
+    if (select instanceof HTMLSelectElement) {
+      entries.push([select.name, select.value]);
+    }
+    const data = Object.fromEntries(entries);
+
+    schema
+      .validate(data)
+      .then(() => {
+        console.log('then');
+        dispatch(addUser(data));
+        navigate('/');
+      })
+      .catch((error) => {
+        const errorMessage = document.querySelector('.form__errors');
+        if (errorMessage) errorMessage.textContent = error.message;
+        console.log('catch', error);
+      });
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
-      <h2 className="form__header">The form with React Hook Form</h2>
+    <form className="form" onSubmit={(event) => onSubmitHandler(event)}>
+      <h2 className="form__header">The form with uncontrolled components</h2>
+      <input placeholder="Your name" type="text" required name="name" />
+
+      <input placeholder="Your age" type="text" required name="age" />
+
       <input
-        {...register('name')}
-        placeholder="Your name"
-        type="text"
-        required
-      />
-      <p className="form__errors">{errors.name?.message}</p>
-      <input {...register('age')} placeholder="Your age" type="text" required />
-      <p className="form__errors">{errors.age?.message}</p>
-      <input
-        {...register('email')}
         placeholder="E-mail"
         type="email"
         autoComplete="username"
+        name="email"
         required
       />
-      <p className="form__errors">{errors.email?.message}</p>
+
       <input
-        {...register('password')}
         placeholder="Password"
         type="password"
         autoComplete="new-password"
+        name="password"
         required
       />
-      <p className="form__errors">{errors.password?.message}</p>
+
       <input
-        {...register('password')}
         placeholder="Password"
         type="password"
         autoComplete="new-password"
+        name="password"
         required
       />
-      <p className="form__errors">{errors.password?.message}</p>
+
       <div className="form__gender">
         <p>Choose your gender</p>
         <label>
-          <input
-            {...register('gender')}
-            type="radio"
-            name="gender"
-            value="Male"
-          />
+          <input type="radio" name="gender" value="Male" />
           Male
         </label>
         <label>
-          <input
-            {...register('gender')}
-            type="radio"
-            name="gender"
-            value="Female"
-          />
+          <input type="radio" name="gender" value="Female" />
           Female
         </label>
-        <p className="form__errors">{errors.gender?.message}</p>
+        <p></p>
       </div>
+
       <label className="form__country">
         Choose country
-        <select {...register('country')}>
+        <select name="country">
           <option value="">Select...</option>
           <option value="USA">USA</option>
           <option value="Canada">Canada</option>
           <option value="France">France</option>
         </select>
       </label>
-      <p className="form__errors">{errors.country?.message}</p>
+
       <label className="form__accept">
-        <input {...register('gender')} type="checkbox" name="acceptTC" />
+        <input type="checkbox" name="acceptTC" />
         Accept T&C
       </label>
-      <p className="form__errors">{errors.acceptTC?.message}</p>
+
+      <p className="form__errors"></p>
+
       <button className="button" type="submit">
         Input info
       </button>
